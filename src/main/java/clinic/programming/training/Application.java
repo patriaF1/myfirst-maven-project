f201c05643d25d291d3c44bd2f1b76487afc56c5
patrick.alexander <patrick.alexander@dnb.no>
2020-05-24 14:56:17 +0200

    Update code for using dependency stringutils

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,8 +2,15 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 
 public class Application {
+	
+	public int countWords(String ipWords){
+		String[] seperateWords= StringUtils.split(ipWords, ' ');
+		return (seperateWords == null) ? 0: seperateWords.length;
+	}
     
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
@@ -23,5 +30,7 @@ public class Application {
 		System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("I have 6 words for you");
+		System.out.println("Word count: " + count);
     }
 }
